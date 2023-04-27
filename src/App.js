3e89ab6944aad51c06c5334ba7ff46eec2b21690
Roberto Flores <fq211776@alumno.udb.edu.sv>
2023-04-26 22:25:49 -0600

    :white_check_mark: estructura vacia navbar/sidebar

--- src/App.js
+++ src/App.js
@@ -6,16 +6,53 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 const App = () => {
+  const activeMenu = true;
+
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
-              <button>
+              <button
+                type="button"
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">sidebar w-0</div>
+          )}
+          ;
+        </div>
+        <div
+          className={
+            activeMenu
+              ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+              : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+          }
+        >
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+            Navbar
+          </div>
+          <div>
+            <Routes>
+              {/* dashboard  */}
+              <Route path="/" element="{<Ecommerce />}" />
+              <Route path="/ecommerce" element="{<Ecommerce />}" />
+
+              {/* pages  */}
+              <Route path="/orders" element="{<Orders />}" />
+              <Route path="/employees" element="{<Employees />}" />
+              <Route path="/customers" element="{<Customers />}" />
+            </Routes>
           </div>
         </div>
       </BrowserRouter>
