c411c969984f519b37e24d3ae8dacc922f8e1fff
Roberto Flores <fq211776@alumno.udb.edu.sv>
2023-04-28 09:39:59 -0600

    :bulb: Avance en construccion de sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
 
 const Sidebar = () => {
-  return <div>Sidebar from component</div>;
+  const activeMenu = true;
+
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware /> <span>PokemonBank</span>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
