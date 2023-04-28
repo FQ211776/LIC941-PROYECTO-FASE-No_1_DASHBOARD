8073f0d012760f6d024e22844c3b0ab9d3bd5d4f
Roberto Flores <fq211776@alumno.udb.edu.sv>
2023-04-28 10:45:03 -0600

    :tada: actualizando sidebard

---
+++ src/data/dummy.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { FiShoppingBag } from "react-icons/fi";
+
+export const links = [
+  {
+    title: "Dashboard",
+    links: [
+      {
+        name: "pokemonbank",
+        icon: <FiShoppingBag />,
+      },
+    ],
+  },
+
+  {
+    title: "Pages",
+    links: [
+      {
+        name: "Pagina 1",
+        icon: <FiShoppingBag />,
+      },
+      {
+        name: "Pagina 2",
+        icon: <FiShoppingBag />,
+      },
+      {
+        name: "Pagina 3s",
+        icon: <FiShoppingBag />,
+      },
+    ],
+  },
+];
