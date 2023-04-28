9baa41e7d5e7ffc1fae63d05c6f35e01484ea233
Roberto Flores <fq211776@alumno.udb.edu.sv>
2023-04-28 12:51:59 -0600

    :tada: NavBar - Iconos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,34 @@ const Navbar = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
   return (
     <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
-      Navbar from component
+      <div className="flex">
+        <NavButton
+          title="Notification"
+          dotColor="rgb(254, 201, 15)"
+          customFunc={() => {}}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => {}}
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Robert
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+      </div>
     </div>
   );
 };
