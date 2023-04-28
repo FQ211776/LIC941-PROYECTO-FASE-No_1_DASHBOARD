be944cdb15d84738e784a3d0438a5d0b8eaeceeb
Roberto Flores <fq211776@alumno.udb.edu.sv>
2023-04-28 12:13:13 -0600

    :bulb: incio trabajo en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Navbar = () => {
-  return <div>Navbar from component</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+  return (
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+      Navbar from component
+    </div>
+  );
 };
 
 export default Navbar;
