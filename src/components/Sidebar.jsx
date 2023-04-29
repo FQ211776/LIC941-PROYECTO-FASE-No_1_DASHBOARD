a2715242c9fa7fabfbed9f02a44b0a1899292caf
Roberto Flores <fq211776@alumno.udb.edu.sv>
2023-04-29 00:02:09 -0600

    :art: Complemeno a NavBat y Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,16 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
+
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, screenSize } =
+    useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
@@ -21,17 +29,16 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {
-                setActiveMenu(false);
-              }}
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Pokemon Bank</span>
+              <SiShopware /> <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
                 onClick={() => setActiveMenu(!activeMenu)}
+                style={{ color: currentColor }}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -48,7 +55,10 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
+                    style={({ isActive }) => ({
+                      backgroundColor: isActive ? currentColor : "",
+                    })}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,5 +1,18 @@
 import React from "react";
-import { FiShoppingBag } from "react-icons/fi";
+import {} from "react-icons/ai";
+import { FiShoppingBag, FiCreditCard } from "react-icons/fi";
+import { BsCurrencyDollar, BsShield } from "react-icons/bs";
+import avatar from "./avatar.jpg";
+import avatar2 from "./avatar2.jpg";
+import avatar3 from "./avatar3.png";
+import avatar4 from "./avatar4.jpg";
+import product1 from "./product1.jpg";
+import product2 from "./product2.jpg";
+import product3 from "./product3.jpg";
+import product4 from "./product4.jpg";
+import product5 from "./product5.jpg";
+import product6 from "./product6.jpg";
+import product7 from "./product7.jpg";
 
 export const links = [
   {
@@ -28,5 +41,50 @@ export const links = [
         icon: <FiShoppingBag />,
       },
     ],
+  },
+];
+
+export const chatData = [
+  {
+    image: avatar2,
+    message: "Cargo Netflix mes de mayo",
+    desc: "Gracias por su pago",
+    time: "9:08 AM",
+  },
+  {
+    image: avatar3,
+    message: "Nuevo pago recibido",
+    desc: "Salma te envio dinero",
+    time: "11:56 AM",
+  },
+  {
+    image: avatar4,
+    message: "Nuevo pago recibido",
+    desc: "Andres te envio dinero",
+    time: "4:39 PM",
+  },
+];
+
+export const userProfileData = [
+  {
+    icon: <BsCurrencyDollar />,
+    title: "Mi Perfil",
+    desc: "Configuracion de la cuenta",
+    iconColor: "#03C9D7",
+    iconBg: "#E5FAFB",
+  },
+  {
+    icon: <BsShield />,
+    title: "Mis Transacciones",
+    desc: "Alertas de movimientoss",
+    iconColor: "rgb(0, 194, 146)",
+    iconBg: "rgb(235, 250, 242)",
+  },
+  {
+    icon: <FiCreditCard />,
+    title: "Pre-calificado",
+    desc: "Productos y servicios pre-calificados",
+    iconColor: "rgb(255, 244, 229)",
+    iconBg: "rgb(254, 201, 15)",
   },
 ];
